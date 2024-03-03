9118efe7330292090560cc78570e7a546ef2925b
Isaac Rivera <isaacrivera2020@u.northwestern.edu>
2024-03-03 12:50:44 -0500

    add pc to start menu, not fully tested but looks good so far

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -48,7 +48,7 @@
 #define FLAG_UNUSED_TOUCHBALLS    0x23 // Unused Flag
 #define FLAG_UNUSED_DAWN     0x24 // Unused Flag
 #define FLAG_UNUSED_ROARK    0x25 // Unused Flag
-#define FLAG_UNUSED_0x026    0x26 // Unused Flag
+#define FLAG_UNUSED_POKEMONPCMENU    0x26 // Unused Flag
 #define FLAG_UNUSED_0x027    0x27 // Unused Flag
 #define FLAG_UNUSED_0x028    0x28 // Unused Flag
 #define FLAG_UNUSED_0x029    0x29 // Unused Flag
