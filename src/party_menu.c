2e3f8c8d3fb2c28544b27470abcfe700b2ab3b6f
Isaac Rivera <isaacrivera2020@u.northwestern.edu>
2024-03-03 17:48:08 -0500

    unlimited TMs, see EVs and IVs in summary

--- src/party_menu.c
+++ src/party_menu.c
@@ -5270,13 +5270,10 @@ static void Task_LearnedMove(u8 taskId)
 {
     struct Pokemon *mon = &gPlayerParty[gPartyMenu.slotId];
     s16 *move = &gPartyMenu.data1;
-    u16 item = gSpecialVar_ItemId;
 
     if (move[1] == 0)
     {
         AdjustFriendship(mon, FRIENDSHIP_EVENT_LEARN_TMHM);
-        if (!ItemId_GetImportance(item))
-            RemoveBagItem(item, 1);
     }
     GetMonNickname(mon, gStringVar1);
     StringCopy(gStringVar2, gMoveNames[move[0]]);
