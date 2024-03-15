d4491e67b65d376792e382cce4365dd58680b41e
Isaac Rivera <isaacrivera2020@u.northwestern.edu>
2024-03-15 10:34:21 -0400

    up level cap

--- src/level_caps.c
+++ src/level_caps.c
@@ -11,13 +11,13 @@ u32 GetCurrentLevelCap(void)
     {
         {FLAG_BADGE01_GET, 15},
         {FLAG_BADGE02_GET, 25},
-        {FLAG_BADGE03_GET, 27},
-        {FLAG_BADGE04_GET, 29},
-        {FLAG_BADGE05_GET, 31},
-        {FLAG_BADGE06_GET, 33},
-        {FLAG_BADGE07_GET, 42},
-        {FLAG_BADGE08_GET, 46},
-        {FLAG_IS_CHAMPION, 58},
+        {FLAG_BADGE03_GET, 34},
+        {FLAG_BADGE04_GET, 44},
+        {FLAG_BADGE05_GET, 47},
+        {FLAG_BADGE06_GET, 50},
+        {FLAG_BADGE07_GET, 52},
+        {FLAG_BADGE08_GET, 54},
+        {FLAG_IS_CHAMPION, 85},
     };
 
     u32 i;
