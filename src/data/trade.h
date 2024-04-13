3f6ad36217e6f6bb141fa8a3ebd5b06cb586e925
Isaac Rivera <isaacrivera2020@u.northwestern.edu>
2024-04-13 12:42:56 -0400

    tweaks through roxanne and disable match call unless rematch (TBD)

--- src/data/trade.h
+++ src/data/trade.h
@@ -984,16 +984,16 @@ static const union AffineAnimCmd *const sAffineAnims_CrossingMonPics[] =
 
 static const struct InGameTrade sIngameTrades[] =
 {
-    [INGAME_TRADE_SEEDOT] =
+    [INGAME_TRADE_MIMR_JR] =
     {
-        .nickname = _("DOTS"),
-        .species = SPECIES_SEEDOT,
-        .ivs = {5, 4, 5, 4, 4, 4},
+        .nickname = _("MARCEL"),
+        .species = SPECIES_MIME_JR,
+        .ivs = {31, 31, 31, 31, 31, 31},
         .abilityNum = 1,
         .otId = 38726,
         .conditions = {30, 5, 5, 5, 5},
         .personality = 0x84,
-        .heldItem = ITEM_CHESTO_BERRY,
+        .heldItem = ITEM_NONE,
         .mailNum = -1,
         .otName = _("KOBE"),
         .otGender = MALE,
