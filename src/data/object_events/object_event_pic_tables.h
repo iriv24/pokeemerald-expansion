12ada05b8467dce3291bb13626f6e4595b4d7f8f
Isaac Rivera <isaacrivera2020@u.northwestern.edu>
2024-05-29 13:16:47 -0400

    shrink cynthia overworld

--- src/data/object_events/object_event_pic_tables.h
+++ src/data/object_events/object_event_pic_tables.h
@@ -1294,15 +1294,15 @@ const struct SpriteFrameImage gObjectEventPicTable_Gardenia[] = {
 };
 
 const struct SpriteFrameImage gObjectEventPicTable_Cynthia[] = {
-    overworld_frame(gObjectEventPic_Cynthia, 4, 4, 0),
-    overworld_frame(gObjectEventPic_Cynthia, 4, 4, 1),
-    overworld_frame(gObjectEventPic_Cynthia, 4, 4, 2),
-    overworld_frame(gObjectEventPic_Cynthia, 4, 4, 3),
-    overworld_frame(gObjectEventPic_Cynthia, 4, 4, 4),
-    overworld_frame(gObjectEventPic_Cynthia, 4, 4, 5),
-    overworld_frame(gObjectEventPic_Cynthia, 4, 4, 6),
-    overworld_frame(gObjectEventPic_Cynthia, 4, 4, 7),
-    overworld_frame(gObjectEventPic_Cynthia, 4, 4, 8),
+    overworld_frame(gObjectEventPic_Cynthia, 2, 4, 0),
+    overworld_frame(gObjectEventPic_Cynthia, 2, 4, 1),
+    overworld_frame(gObjectEventPic_Cynthia, 2, 4, 2),
+    overworld_frame(gObjectEventPic_Cynthia, 2, 4, 0),
+    overworld_frame(gObjectEventPic_Cynthia, 2, 4, 0),
+    overworld_frame(gObjectEventPic_Cynthia, 2, 4, 1),
+    overworld_frame(gObjectEventPic_Cynthia, 2, 4, 1),
+    overworld_frame(gObjectEventPic_Cynthia, 2, 4, 2),
+    overworld_frame(gObjectEventPic_Cynthia, 2, 4, 2),
 };
 
 const struct SpriteFrameImage gObjectEventPicTable_Wake[] = {
