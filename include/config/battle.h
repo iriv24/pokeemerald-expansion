833ef476afc003ff12aa5b9fb331cc06f20be556
Isaac Rivera <isaacrivera2020@u.northwestern.edu>
2024-10-20 17:23:31 -0400

    turn off shadows

--- include/config/battle.h
+++ include/config/battle.h
@@ -283,6 +283,6 @@
 #define B_SHOW_TYPES        SHOW_TYPES_ALWAYS // When defined as SHOW_TYPES_ALWAYS, after selecting "Fight" in battle, the types of all Pokemon are revealed. Whe defined as SHOW_TYPES_OWN, types are only revealed if the player owns the mon in question.
 
 // Pokémon battle sprite settings
-#define B_ENEMY_MON_SHADOW_STYLE        GEN_LATEST // In Gen4+, all enemy Pokemon will have a shadow drawn beneath them.
+#define B_ENEMY_MON_SHADOW_STYLE        GEN_3 // In Gen4+, all enemy Pokemon will have a shadow drawn beneath them.
 
 #endif // GUARD_CONFIG_BATTLE_H
