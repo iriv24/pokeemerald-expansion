e1854e98a2e8233ce5f71e0f05df0dcd5448d783
Isaac Rivera <isaacrivera2020@u.northwestern.edu>
2025-01-29 21:32:25 -0500

    fix dexnav search levels

--- src/pokemon.c
+++ src/pokemon.c
@@ -6855,6 +6855,11 @@ void HandleSetPokedexFlag(u16 nationalNum, u8 caseId, u32 personality)
         if (NationalPokedexNumToSpecies(nationalNum) == SPECIES_SPINDA)
             gSaveBlock2Ptr->pokedex.spindaPersonality = personality;
     }
+
+    if (caseId == FLAG_SET_SEEN)
+    {
+        TryIncrementSpeciesSearchLevel(nationalNum);
+    }
 }
 
 bool8 HasTwoFramesAnimation(u16 species)
