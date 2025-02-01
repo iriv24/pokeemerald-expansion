152a40d0325eef2edd0c7ece6d327c927808eaf2
Isaac Rivera <isaacrivera2020@u.northwestern.edu>
2025-02-01 11:30:43 -0500

    fix potential softlock if you could make it to briney's house without fighting rustboro rival

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -266,7 +266,7 @@
 #define VAR_RIVAL_RAYQUAZA_CALL_STEP_COUNTER             0x40F6
 #define VAR_STARTER_GEN                                  0x40F7
 #define VAR_UNUSED_0x40F8                                0x40F8 // Unused Var probably
-#define VAR_FOUGHT_DAWN_104                              0x40F9
+#define VAR_UNUSED_0x40F9                                0x40F9 // Unused Var
 #define VAR_UNUSED_0x40FA                                0x40FA // Unused Var
 #define VAR_BEAT_WATTSON                                 0x40FB
 #define VAR_FOUGHT_DAWN_111                              0x40FC 
