7ffedf32e59edad5ee0d82ee46dcff4c98c93fa0
Dana Yatsuta <danayatsuta@gmail.com>
2025-02-14 18:21:24 +0300

    Disable checks for dev

--- include/user_protection.h
+++ include/user_protection.h
@@ -4,8 +4,8 @@
 #define SLOW_HASH 1
 #define FAST_HASH 4
 
-#define SHOULD_RUN_ROM_CHECK TRUE       //  Verify that the patching was successful
-#define SHOULD_RUN_RAM_CHECK TRUE       //  Verify that no savestate has been carried over
+#define SHOULD_RUN_ROM_CHECK FALSE       //  Verify that the patching was successful
+#define SHOULD_RUN_RAM_CHECK FALSE       //  Verify that no savestate has been carried over
 #define ROM_HASH_SPEED       FAST_HASH
 
 extern bool8 gPatchSuccess;
